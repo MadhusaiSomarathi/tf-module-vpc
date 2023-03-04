819e5fdf1b6bcf2fd6b815e733774df11e96cf09
rthootika <msamrat789@gmail.com>
2023-03-04 12:57:52 +0530

    Updates

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
-#output "private_subnets" {
-#  value = module.private_subnets
-#}
-#
-#output "public_subnets" {
-#  value = module.public_subnets
-#}
-#
-#output "all_route_tables" {
-#  value = local.all_route_tables
-#
-#}
-#
+output "private_subnets" {
+  value = module.private_subnets
+}
+
+output "public_subnets" {
+  value = module.public_subnets
+}
+
+output "all_route_tables" {
+  value = local.all_route_tables
+
+}
+
 output "test" {
   value = module.public_subnets
 }
 
-#output "vpc_id" {
-#  value = aws_vpc.main.id
-#}
-#
-#output "vpc_cidr" {
-#  value = aws_vpc.main.cidr_block
-#}
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.main.cidr_block
+}
 
