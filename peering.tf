45158afeee56c49c6e52728634310ff2f8a60258
rthootika <msamrat789@gmail.com>
2023-03-05 14:26:03 +0530

    Updates

--- peering.tf
+++ peering.tf
@@ -4,3 +4,10 @@ resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
   auto_accept = true
   tags        = local.vpc_peering_tags
 }
+
+resource "aws_route" "route_from_default_management_route_table" {
+  count                     = length(local.all_route_tables) //4
+  route_table_id            = element(local.all_route_tables, count.index)
+  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.id
+}
