0b6469b3b0ebd1b6b5a87ae710a1f3b59b478436
rthootika <msamrat789@gmail.com>
2023-03-05 13:29:15 +0530

    Updates

--- subnets/main.tf
+++ subnets/main.tf
@@ -6,20 +6,20 @@ resource "aws_subnet" "main" {
   availability_zone = element(var.subnet_availability_zones, count.index) //(["us-east-1a", "us-east-1b"],0) (["us-east-1a", "us-east-1b"],1)
 }
 
-#resource "aws_route_table" "aws_route_table" {
-#  vpc_id = var.vpc_id
-#  tags = {
-#    Name    = "${var.env}-${var.name}-rt"
-#    ENV     = var.env
-#    PROJECT = "roboshop"
-#  }
-#}
-#
-#resource "aws_route_table_association" "route-table-association" {
-#  count          = length(aws_subnet.main) //2
-#  subnet_id      = element(aws_subnet.main.*.id, count.index) //(["subnet-0a74919a7fd5254da","subnet-08d1fa9f0d40bc061"],0) (["subnet-0a74919a7fd5254da","subnet-08d1fa9f0d40bc061"],1)
-#  route_table_id = aws_route_table.aws_route_table.id
-#}
+resource "aws_route_table" "aws_route_table" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name    = "${var.env}-${var.name}-rt"
+    ENV     = var.env
+    PROJECT = "roboshop"
+  }
+}
+
+resource "aws_route_table_association" "route-table-association" {
+  count          = length(aws_subnet.main) //2
+  subnet_id      = element(aws_subnet.main.*.id, count.index) //(["subnet-0a74919a7fd5254da","subnet-08d1fa9f0d40bc061"],0) (["subnet-0a74919a7fd5254da","subnet-08d1fa9f0d40bc061"],1)
+  route_table_id = aws_route_table.aws_route_table.id
+}
 
 #resource "aws_route" "internet_gateway_route" {
 #  route_table_id         = aws_route_table.aws_route_table.id
