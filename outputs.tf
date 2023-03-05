01a2d38c3eba5588b49aa5936d59689847f75760
rthootika <msamrat789@gmail.com>
2023-03-05 12:47:50 +0530

    Updates

--- outputs.tf
+++ outputs.tf
@@ -5,17 +5,21 @@
 #output "public_subnets" {
 #  value = module.public_subnets
 #}
-
+#
 #output "all_route_tables" {
 #  value = local.all_route_tables
 #
 #}
 
+output "test" {
+  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+}
+
 #output "vpc_id" {
-#  value =
+#  value = aws_vpc.main.id
 #}
 #
 #output "vpc_cidr" {
 #  value = aws_vpc.main.cidr_block
 #}
-#
+
