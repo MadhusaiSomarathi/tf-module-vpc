77a0007d8a6dda0ca450a42bab92adac0d7a0c32
rthootika <msamrat789@gmail.com>
2023-03-05 14:24:14 +0530

    Updates

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,18 @@
 output "private_subnets" {
-  value = [for i, j in module.private_subnets : j.subnets.*.id]
+  value = module.private_subnets
 }
 
 output "public_subnets" {
   value = module.public_subnets
 }
-#
-#output "all_route_tables" {
-#  value = local.all_route_tables
-#
-#}
 
-#output "test" {
-#  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
-#}
+output "all_route_tables" {
+  value = local.all_route_tables
+}
+
+output "test" {
+  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+}
 
 output "vpc_id" {
   value = aws_vpc.main.id
